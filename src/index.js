fc74830594a9c1247f923da714e1b67aa9ada6d6
Max Rozen <rozenmd@gmail.com>
2017-11-08 09:22:47 +1100

    Use a real query

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // 2
-const httpLink = new HttpLink({ uri: '' })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9pidiad2dje01183c6xnmtd' })
 
 // 3
 const client = new ApolloClient({
